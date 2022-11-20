08feec1a541a782c40b459177d58ac2771fc526c
AaroHuttunen <aaro.huttunen@outlook.com>
2022-11-20 19:44:06 +0200

    A quick example of a slider which stores it value in a state and displays the value after slider has stopped moving.

--- 1C/src/App.js
+++ 1C/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles/App.css";
+import ReactSlider from "react-slider";
+import { useState } from "react";
 
 function App() {
+
+  const [currentValue, setCurrentValue] = useState(0);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactSlider
+      className="horizontal-slider"
+      thumbClassName="example-thumb"
+      trackClassName="example-track"
+      min={0}
+      max={100}
+      defaultValue={0}
+      value={currentValue}
+      onAfterChange={(value) => setCurrentValue(value)}
+    />
+    Value is {currentValue}
     </div>
   );
 }
